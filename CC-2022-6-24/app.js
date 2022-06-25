46311fd51d1d25bbbab986069f8addd3e6e00629
Virginia Gutierrez <virginiagutierrez@Virginias-MacBook-Pro.local>
2022-06-25 14:03:26 -0600

    oding challenge for 06-25-2022

---
+++ CC-2022-6-24/app.js
@@ -0,0 +1,23 @@
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+// first attempt 
+
+// p(parameters): No Parameters
+// r(return):The return is "Hello World"
+// e(example):33=>"Hello World"
+// e: Bobba=> " Hello world"
+// p():Function takes in and returns "Hello World"
+
+
+Function Greet(x){ 
+
+    let x = "Hello World"
+
+return= "Hello World"
+
+}
+
